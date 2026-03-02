702cce239c85a7959d785c601e545a309855022c
DukTai <nguyenductai.12a2tqk@gmail.com>
2026-03-02 10:07:48 +0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-require('dotenv').config(); 
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./src/config/db'); 
-const authRoutes = require('./src/routes/auth.routes');
-const jarProfileRoutes = require('./src/routes/jarprofile.routes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+const authRoutes = require("./src/routes/auth.routes");
+const jarProfileRoutes = require("./src/routes/jarprofile.routes");
 
 const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/jar-profiles', jarProfileRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/jar-profiles", jarProfileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Backend đang chạy tại: http://localhost:${PORT}`);
+  console.log(`Backend đang chạy tại: http://localhost:${PORT}`);
 });
